7fc35daa864211c3efd4bfca9bf3e834321296ef
Cisco Ferre <ferrecisco@icloud.com>
2022-11-08 18:52:18 -0500

    schema logic complete

---
+++ server/schemas/typedefs.js
@@ -0,0 +1,49 @@
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User
+    removeBook(bookId: String!): User
+  }
+
+  input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`; 
+// export these typeDefs
+
+module.exports = typeDefs;
